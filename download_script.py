f1ab3300ad350bd74d950e958ec44908d266a30d
coder1 <coder1@example.com>
2023-05-08 01:14:19 +0300

    Linked processes in two scripts

--- download_script.py
+++ download_script.py
@@ -2,30 +2,41 @@ import os
 import subprocess
 from pytube import YouTube
 import time
+import sys
+import uuid
+import re
 
 time1 = time.time()
 
-link = "https://www.youtube.com/watch?v=MKihQV18qA4"  # Ссылка на видео
-orr = 'mp3' # mp3 или mp4
-
+link = str(sys.argv[1])  # Ссылка на видео
+orr = str(sys.argv[2])  # mp3 или mp4
 yt = YouTube(link)  # Cоздадим объект класса YouTube, и в качестве значения передадим введённую пользователем ссылку
 
-if orr == 'mp4': # если запросили видео
-    ys = yt.streams.get_highest_resolution() # Получим максимально возможное разрешение видео
+def sanitize_filename(filename: str) -> str:
+    """Заменяет ненужные символы на _"""
+    return re.sub(r"[^\w\-_ ]", "_", filename.replace(" ", "_"))
 
-    ys.download('C:/Users/bkree/Desktop') # Загружаем
+if orr == 'mp4':  # если запросили видео
+    ys = yt.streams.get_highest_resolution()  # Получим максимально возможное разрешение видео
+
+    # Загружаем, используя очищенное название файла с расширением .mp4
+    sanitized_title = sanitize_filename(yt.title)
+    ys.download('C:\\Users\\RYZEN9\\Videos', filename=f"{sanitized_title}.mp4")
+
     print("Загрузка завершена!")
-    print("--- %s seconds ---" % (time.time() - time1)) # Время загрузки
+    print("--- %s seconds ---" % (time.time() - time1))  # Время загрузки
+
+
 # если запросили аудио
 else:
     # извлекаем только аудио
     video = yt.streams.filter(only_audio=True).first()
 
     # проверяем пункт назначения для сохранения файла
-    destination = 'C:/Users/bkree/Desktop' or '.'
+    destination = 'C:\\Users\\RYZEN9\\Videos'
 
     # загружаем файл
-    out_file = video.download(output_path=destination)
+    out_file = video.download(output_path=destination, filename=sanitize_filename(yt.title))
 
     # сохраняем файл
     base, ext = os.path.splitext(out_file)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,15 @@
 import re
+import os
 import logging
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import subprocess
+import time
+
+import pytube
+from pytube import YouTube
+from telegram import ForceReply, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
+
+
 
 # Включение логирования
 logging.basicConfig(
@@ -16,10 +24,47 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         rf"Вы можете загрузить любое видео с YouTube или TikTok. Просто отправьте ссылку на него:",
         reply_markup=ForceReply(selective=True),
     )
+    await send_format_choice(update)
+
+async def handle_format_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    query.answer()
+
+    # Сохраните выбранный формат в пользовательских данных
+    context.user_data["format"] = query.data
+    await query.edit_message_text(text=f"Выбранный формат: {query.data}. Теперь отправьте ссылку на видео.")
+
+
+async def handle_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Обрабатывает команды."""
+    command = update.message.text.lower()
+    if command == "/mp3":
+        context.user_data["format"] = "mp3"
+        await update.message.reply_text("Выбран формат mp3. Отправьте ссылку на видео для загрузки.")
+    elif command == "/mp4":
+        context.user_data["format"] = "mp4"
+        await update.message.reply_text("Выбран формат mp4. Отправьте ссылку на видео для загрузки.")
+    else:
+        await update.message.reply_text("Неверная команда. Используйте /mp3 или /mp4 для выбора формата загрузки.")
+
+    # Отправить сообщение с выбранным форматом
+    format = context.user_data.get("format", "mp4")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Отправляет сообщение, когда пользователь вводит команду /help."""
     await update.message.reply_text("Вы можете загрузить любое видео с YouTube или TikTok! Просто скинь ссылку на видео")
+
+async def send_format_choice(update: Update) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("MP4 (Видео)", callback_data="mp4"),
+            InlineKeyboardButton("MP3 (Аудио)", callback_data="mp3"),
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Выберите формат для загрузки видео:", reply_markup=reply_markup)
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Функция эхо, не используется."""
@@ -31,6 +76,11 @@ async def is_link(text: str) -> bool:
         return True
     else:
         return False
+
+def sanitize_filename(filename: str) -> str:
+    """Заменяет ненужные символы на _"""
+    return re.sub(r"[^\w\-_ ]", "_", filename.replace(" ", "_"))
+
 
 async def log_message(chat_id: int, username: str, text: str) -> None:
     """Логирует сообщения от пользователей."""
@@ -48,18 +98,43 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
     if await is_link(text):
         logger.info("Получена ссылка!")
-        await update.message.reply_text("OK")
+        # Запуск download_script.py с аргументами в виде ссылки и формата
+        desired_format = context.user_data.get("format", "mp3")
+        result = subprocess.run(["python", "download_script.py", text, desired_format], capture_output=True, text=True, encoding="utf-8")
+        if result.returncode == 0:
+            await update.message.reply_text("Файл загружен. Отправляю вам его...")
+
+            # Получение тайтла видео
+            yt = YouTube(text)
+            video_title = sanitize_filename(yt.title)
+
+            # Поиск пути файла на основе полученных форматов
+            if desired_format == "mp3":
+                file_name = video_title + ".mp3"
+            else:
+                file_name = video_title + ".mp4"
+
+            file_path = os.path.join("C:\\Users\\RYZEN9\\Videos", file_name)
+
+            # Отправка видео пользователю
+            with open(file_path, "rb") as f:
+                await context.bot.send_document(chat_id=chat_id, document=f)
+
+            # удаление загруженного файла
+            os.remove(file_path)
+        else:
+            await update.message.reply_text("Ошибка при обработке ссылки. Попробуйте еще раз.")
     else:
         await update.message.reply_text("Это не ссылка. Пожалуйста, отправьте мне ссылку на видео YouTube или TikTok.")
 
-def main() -> None:
-    """Запускает   бота."""
-    # Создание приложения и передача ему токена бота.
-    application = Application.builder().token("082210671:AAEy0qYiVCkXzdiTNDNaLjvUFPgi2W6i-4U").build()
 
-    # Обработка различных команд
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+def main() -> None:
+    """Запускает бота."""
+    # Создание приложения и передача ему токена бота.
+    application = Application.builder().token("6082210671:AAEy0qYiVCkXzdiTNDNaLjvUFPgi2W6i-4U").build()
+
+    # Обработка команд
+    application.add_handler(CommandHandler(["mp3", "mp4"], handle_command))
 
     # Обработка всех текстовых сообщений, не являющихся командами
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
